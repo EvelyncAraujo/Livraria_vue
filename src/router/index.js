9ab4e99b728163b9fc0c5c5d6bb4233af0221a23
evelyncaraujo <evyaraujoifc@gmail.com>
2024-03-26 10:47:07 -0300

    cmt- inicial

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,32 @@
-import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
-import CategoriaView from "../views/CategoriaView.vue";
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
+import CategoriaView from '../views/CategoriaView.vue';
+import EditoraView from '../views/EditoraView.vue';
+import AutorView from '../views/AutorView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: "/",
-      name: "home",
+      path: '/',
+      name: 'home',
       component: HomeView,
     },
     {
-      path: "/categorias",
-      name: "categorias",
+      path: '/categorias',
+      name: 'categorias',
       component: CategoriaView,
     },
+    {
+      path: '/editoras',
+      name: 'editoras',
+      component: EditoraView,
+    },
+    {
+      path: '/autores',
+      name: 'autores',
+      component: AutorView
+    }
   ],
 });
 
